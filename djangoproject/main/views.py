02667951b498bbc8cde3e078a84de976290140fa
LizaSozinova <pu07.10.2004@gmail.com>
2024-04-02 15:15:53 +0700

    123

---
+++ djangoproject/main/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from goods.models import Categories
+
+def index(request):
+
+    context: dict={
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME',
+    }
+
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context: dict = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'ЛЯ ЛЯ ЛЯ ЛЯ ЛЯ ЛЯ ЛЯ'
+    }
+
+    return render(request, 'main/about.html', context)
+
